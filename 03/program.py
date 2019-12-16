f00b9be961ac55d05503be03b37f24caa0ca9e45
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2019-12-16 11:52:56 +0000

    Implement fastest intersection

--- 03/program.py
+++ 03/program.py
@@ -1,13 +1,14 @@
 
-from wiregrid import Wire, nearest_intersection
+from wiregrid import Wire, nearest_intersection, fastest_intersection
 
 def main():
 
   paths = open("./wire_grid.txt").readlines()
   wire_1 = paths[0]
-  wire_2 = paths[1]  
+  wire_2 = paths[1]
 
-  print(nearest_intersection(wire_1, wire_2))
+  print("NEAREST :", nearest_intersection(wire_1, wire_2))
+  print("FASTEST :", fastest_intersection(wire_1, wire_2))
 
 
 main()
