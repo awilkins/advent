f2853109f6fddf2ee4e218938a8ccf3533b5f042
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2019-12-16 13:11:12 +0000

    Changes for part 2

--- 04/password_test.py
+++ 04/password_test.py
@@ -5,10 +5,15 @@ from password import check
 class TestPassword(unittest.TestCase):
 
   def test_all_ones(self):
-    self.assertTrue(check("111111"))
+    self.assertFalse(check("111111"))
 
   def test_descending(self):
     self.assertFalse(check("223450"))
 
   def test_no_double(self):
     self.assertFalse(check("123789"))
+
+  def test_no_long_runs(self):
+    self.assertTrue(check("112233"))
+    self.assertTrue(check("111122"))
+    self.assertFalse(check("123444"))
