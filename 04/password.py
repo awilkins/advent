ba479092b00e53c0af81093af652027d7f0650bc
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2019-12-16 13:11:12 +0000

    Changes for part 2

--- 04/password.py
+++ 04/password.py
@@ -6,9 +6,18 @@ def ascends(password):
   return True
 
 def has_double(password):
+  digit_count = {}
   for index in range(1, len(password)):
     if password[index - 1] == password[index]:
+      if not password[index] in digit_count:
+        digit_count[password[index]] = 2
+      else:
+        digit_count[password[index]] = digit_count[password[index]] + 1
+
+  for v in digit_count.values():
+    if v == 2:
       return True
+
   return False
 
 def check(password):
