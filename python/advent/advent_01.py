5c90d267641d905ca942566a0a2e79c000ed8add
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2020-12-01 17:19:47 +0000

    Python solution

---
+++ python/advent/advent_01.py
@@ -0,0 +1,36 @@
+
+def expense_anomaly(expenses: str) -> int:
+  expense_lines = [ int(line) for line in expenses.splitlines() ]
+
+  for line_no in range(len(expense_lines)):
+    for other_line_no in range(len(expense_lines)):
+      
+      if line_no == other_line_no:
+        continue
+
+      a = expense_lines[line_no]
+      b = expense_lines[other_line_no]
+
+      if a + b == 2020:
+        return a * b
+
+def expense_anomaly_triple(expenses: str) -> int:
+  expense_lines = [ int(line) for line in expenses.splitlines() ]
+  
+  for line_no in range(len(expense_lines)):
+    for other_line_no in range(len(expense_lines)):
+      for yet_another_line_no in range(len(expense_lines)):
+      
+        if line_no == other_line_no or \
+           line_no == yet_another_line_no or \
+           other_line_no == yet_another_line_no:
+          continue
+
+        a = expense_lines[line_no]
+        b = expense_lines[other_line_no]
+        c = expense_lines[yet_another_line_no]
+
+        if a + b + c == 2020:
+          return a * b * c
+
+  
