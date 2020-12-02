74e9cb3960aa4c92517168921c11d0a85eac7319
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2020-12-02 15:44:00 +0000

    Add editorconfig and autopep8
    Grr

--- python/advent/day_01/advent_01.py
+++ python/advent/day_01/advent_01.py
@@ -1,36 +1,35 @@
 
 def expense_anomaly(expenses: str) -> int:
-  expense_lines = [ int(line) for line in expenses.splitlines() ]
+    expense_lines = [int(line) for line in expenses.splitlines()]
 
-  for line_no in range(len(expense_lines)):
-    for other_line_no in range(len(expense_lines)):
-      
-      if line_no == other_line_no:
-        continue
+    for line_no in range(len(expense_lines)):
+        for other_line_no in range(len(expense_lines)):
 
-      a = expense_lines[line_no]
-      b = expense_lines[other_line_no]
+            if line_no == other_line_no:
+                continue
 
-      if a + b == 2020:
-        return a * b
+            a = expense_lines[line_no]
+            b = expense_lines[other_line_no]
+
+            if a + b == 2020:
+                return a * b
+
 
 def expense_anomaly_triple(expenses: str) -> int:
-  expense_lines = [ int(line) for line in expenses.splitlines() ]
-  
-  for line_no in range(len(expense_lines)):
-    for other_line_no in range(len(expense_lines)):
-      for yet_another_line_no in range(len(expense_lines)):
-      
-        if line_no == other_line_no or \
-           line_no == yet_another_line_no or \
-           other_line_no == yet_another_line_no:
-          continue
+    expense_lines = [int(line) for line in expenses.splitlines()]
 
-        a = expense_lines[line_no]
-        b = expense_lines[other_line_no]
-        c = expense_lines[yet_another_line_no]
+    for line_no in range(len(expense_lines)):
+        for other_line_no in range(len(expense_lines)):
+            for yet_another_line_no in range(len(expense_lines)):
 
-        if a + b + c == 2020:
-          return a * b * c
+                if line_no == other_line_no or \
+                   line_no == yet_another_line_no or \
+                   other_line_no == yet_another_line_no:
+                    continue
 
-  
+                a = expense_lines[line_no]
+                b = expense_lines[other_line_no]
+                c = expense_lines[yet_another_line_no]
+
+                if a + b + c == 2020:
+                    return a * b * c
