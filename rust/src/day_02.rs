42adfbea09d2c8e2165a9026b4290befc0985af4
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2020-12-06 13:33:34 +0000

    Bits of in-progress stuff

---
+++ rust/src/day_02.rs
@@ -0,0 +1,9 @@
+
+
+pub fn answer_1() -> i32 {
+  return 0;
+}
+
+pub fn answer_2() -> i32 {
+  return 0;
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,15 +1,33 @@
 use std::fs;
 
 mod day_01;
+mod day_02;
+mod util;
 
 fn main() {
-    match fs::read_to_string("../resources/day_01/input_01.txt") {
-        Ok(expenses) => {
-            println!("Answer 1 : {}", day_01::expense_anomaly(&expenses));
-            println!("Answer 2 : {}", day_01::expense_anomaly_triple(&expenses));
-        }
-        Err(why) => {
-            print!("{}", why);
-        }
+  let RESOURCE = "day_01/input_01.txt";
+  let expenses = util::get_resource(&RESOURCE);
+  println!("Answer 2 : {}", day_01::expense_anomaly_triple(&expenses));
+
+
+  // match fs::read_to_string("../resources/day_01/input_01.txt") {
+  //   Ok(expenses) => {
+  //     println!("Answer 1 : {}", day_01::expense_anomaly(&expenses));
+  //     println!("Answer 2 : {}", day_01::expense_anomaly_triple(&expenses));
+  //   }
+  //   Err(why) => {
+  //     print!("{}", why);
+  //   }
+  // }
+
+  match fs::read_to_string("../resources/day_02/input.txt") {
+    Ok(expenses) => {
+      println!("Day 02");
+      println!("\tAnswer 1 : {}", day_02::answer_1());
+      println!("\tAnswer 2 : {}", day_02::answer_2());
     }
+    Err(why) => {
+      print!("{}", why);
+    }
+  }
 }
