388e9b70e48dc26f9dba49bf3956abbabab9cdc8
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2020-12-11 12:44:35 +0000

    Script to make and open a day's code

---
+++ templates/python/test_code_foot.py
@@ -0,0 +1,30 @@
+
+EXAMPLE_INPUT = """\
+"""
+
+class TestThing(TestCase):
+
+    def test_example_1(self):
+        input_lines = EXAMPLE_INPUT.splitlines()
+
+        expected =
+        actual =
+
+        self.assertEqual(expected, actual)
+
+    # def test_answer_1(self):
+    #     input_lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
+
+    #     # expected =
+    #     answer =
+    #     # self.assertEqual(expected, answer)
+    #     print(f'\nAnswer 1 : {answer}\n')
+
+    # def test_answer_2(self):
+    #     input_lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
+
+    #     # expected =
+    #     answer =
+    #     # self.assertEqual(expected, answer)
+    #     print(f'\nAnswer 2 : {answer}\n')
+
