be4f068346e7c4f35b19cdcc8474f2f56224b163
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2020-12-15 08:36:25 +0000

    Ok then, not too slow

--- python/advent/day_15/memory_game.py
+++ python/advent/day_15/memory_game.py
@@ -37,6 +37,30 @@ def play(number_list: str, max_count=2020):
 
     while count < max_count:
         count += 1
+        if count % 1000000 == 0:
+            print(count)
         last_number = speak(last_number, numbers, count)
 
     return last_number
+
+
+def main():
+
+    tests = [
+        ("2,0,1,9,5,19", 1), # my puzzle input
+        # ("0,3,6", 175594),
+        # ("1,3,2", 2578),
+        # ("2,1,3", 3544142),
+        # ("1,2,3", 261214),
+        # ("2,3,1", 6895259),
+        # ("3,2,1", 18),
+        # ("3,1,2", 362),
+    ]
+
+    for numbers, expected in tests:
+        print("HOORAY")
+        print(numbers, play(numbers, 30000000))
+
+
+if "__main__" == __name__:
+    main()
