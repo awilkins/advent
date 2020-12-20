b3d702a65668f3945a6b8fa82763af115a2a6799
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2020-12-20 11:05:30 +0000

    Day 20 part 1

---
+++ python/advent/day_20/jigsaw.py
@@ -0,0 +1,98 @@
+
+
+from typing import Dict, List, Set
+
+
+TILE_SIZE = 10
+
+
+BITS = str.maketrans('.#', '01')
+
+def pixels_int(pixels: str) -> int:
+    bits = pixels.translate(BITS)
+    return int(bits, 2)
+
+
+def flipped(value: int, bitsize: int = TILE_SIZE) -> int:
+    bits = bin(value)
+    flipped_bits = bits[2:].zfill(bitsize)[::-1]
+    return int(flipped_bits, 2)
+
+
+class Tile:
+
+    def __init__(self, lines: List[str]) -> None:
+        assert len(lines) == TILE_SIZE + 1
+        self.id = int(lines[0].split(' ')[1][:-1])
+        self.pixels = lines[1:]
+
+        # borders
+        #|-0->
+        #|   |
+        #3   1
+        #|   v
+        #v-2->
+        self.borders: List[int] = []
+        self.borders.append(pixels_int(self.pixels[0]))
+        self.borders.append(pixels_int(
+            "".join([
+                self.pixels[y][9] for y in range(10)
+            ])
+        ))
+        self.borders.append(pixels_int(self.pixels[9]))
+        self.borders.append(pixels_int(
+            "".join([
+                self.pixels[y][0] for y in range(10)
+            ])
+        ))
+        self.flipped_borders = list([
+            flipped(border) for border in self.borders
+        ])
+
+class Puzzle:
+
+    def __init__(self, lines: List[str]) -> None:
+
+        ii = 0
+        self.tiles: Dict[int, Tile] = {}
+        while ii < len(lines):
+            tile_lines = []
+            line = lines[ii]
+            while len(line) > 0:
+                tile_lines.append(line)
+                ii += 1
+                line = lines[ii]
+            tile = Tile(tile_lines)
+            self.tiles[tile.id] = tile
+            ii += 1
+
+
+    def find_corners(self) -> List[Tile]:
+
+        corners = []
+        # Do I have 2 borders that don't match other tiles?
+        for index, tile in self.tiles.items():
+            unmatched_count = 0
+            for border in tile.borders:
+                matched = False
+                for other_index, other_tile in self.tiles.items():
+                    if index == other_index:
+                        continue
+                    if border in other_tile.borders:
+                        matched = True
+                        break
+                    if border in other_tile.flipped_borders:
+                        matched = True
+                        break
+
+                if not matched:
+                    unmatched_count += 1
+            if unmatched_count == 2:
+                corners.append(tile)
+
+        return corners
+
+
+
+
+
---
+++ python/tests/day_20/test_jigsaw.py
@@ -0,0 +1,100 @@
+from unittest import TestCase
+
+from ..util import get_resource
+
+DAY="20"
+
+from advent.day_20.jigsaw import *
+
+TILE_2311 = """\
+Tile 2311:
+..##.#..#.
+##..#.....
+#...##..#.
+####.#...#
+##.##.###.
+##...#.###
+.#.#.#..##
+..#....#..
+###...#.#.
+..###..###
+"""
+
+class TestThing(TestCase):
+
+    def test_bitflip(self):
+
+        expected = int('1000000000', 2)
+        actual = flipped(1)
+        self.assertEqual(expected, actual)
+
+
+    def test_pixels_int(self):
+        expected = 1
+        actual = pixels_int('.........#')
+
+        self.assertEqual(expected, actual)
+
+
+    def test_tile(self):
+
+        tile = Tile(TILE_2311.splitlines())
+
+        self.assertEqual(2311, tile.id)
+        self.assertListEqual([
+            210,
+            89,
+            231,
+            498,
+        ], tile.borders)
+
+
+    def test_puzzle(self):
+        lines = get_resource(f'day_{DAY}/example_1.txt').read_text().splitlines()
+        puzzle = Puzzle(lines)
+
+        self.assertEqual(9, len(puzzle.tiles))
+
+        self.assertListEqual([
+            2311, 1951, 1171, 1427, 1489, 2473, 2971, 2729, 3079
+        ], list(puzzle.tiles.keys()))
+
+    def test_example_1(self):
+        lines = get_resource(f'day_{DAY}/example_1.txt').read_text().splitlines()
+        puzzle = Puzzle(lines)
+
+        corners = puzzle.find_corners()
+        self.assertEqual(4, len(corners))
+        product = 1
+        for tile in corners:
+            product *= tile.id
+
+        self.assertEqual(20899048083289, product)
+
+    def test_answer_1(self):
+        lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
+
+        puzzle = Puzzle(lines)
+
+        corners = puzzle.find_corners()
+        self.assertEqual(4, len(corners))
+        product = 1
+
+        for tile in corners:
+            product *= tile.id
+
+        answer = product
+        print(f'\nAnswer 1 : {answer}\n')
+
+        # expected =
+        # self.assertEqual(expected, answer)
+
+    # def test_answer_2(self):
+    #     input_lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
+
+    #     answer =
+    #     print(f'\nAnswer 2 : {answer}\n')
+
+    #     # expected =
+    #     # self.assertEqual(expected, answer)
+
