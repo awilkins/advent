dc190d76a1ab98325fffa9ddcccc5c4e811bd98d
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2021-12-04 09:15:59 +0000

    Day 4 initial solution

---
+++ python/tests/day_04/test_bingo.py
@@ -0,0 +1,58 @@
+from unittest import TestCase
+
+from ..util import get_resource
+
+DAY="04"
+
+from advent.day_04.bingo import *
+
+EXAMPLE_INPUT = """\
+7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7
+"""
+
+class TestThing(TestCase):
+
+    def test_example_1(self):
+        lines = EXAMPLE_INPUT.splitlines()
+
+        expected = 4512
+        actual = answer_1(lines)
+
+        self.assertEqual(expected, actual)
+
+    def test_answer_1(self):
+        lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
+
+        answer = answer_1(lines)
+        print(f'\nAnswer 1 : {answer}\n')
+
+        # expected =
+        # self.assertEqual(expected, answer)
+
+    def test_answer_2(self):
+        lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
+
+        answer = answer_2(lines)
+        print(f'\nAnswer 2 : {answer}\n')
+
+        # expected =
+        # self.assertEqual(expected, answer)
+
