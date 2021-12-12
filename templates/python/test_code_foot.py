4c87cef9309409e447ae5934ca82f9fc3748f455
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2021-12-12 11:08:39 +0000

    Update template to make edits faster

--- templates/python/test_code_foot.py
+++ templates/python/test_code_foot.py
@@ -6,27 +6,27 @@ class TestThing(TestCase):
 
     def test_example_1(self):
         lines = EXAMPLE_INPUT.splitlines()
-
         expected =
         actual =
-
         self.assertEqual(expected, actual)
+
+    # def test_example_2(self):
+    #     lines = EXAMPLE_INPUT.splitlines()
+    #     expected =
+    #     actual =
+    #     self.assertEqual(expected, actual)
 
     # def test_answer_1(self):
     #     lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
-
     #     answer =
     #     print(f'\nAnswer 1 : {answer}\n')
-
     #     # expected =
     #     # self.assertEqual(expected, answer)
 
     # def test_answer_2(self):
     #     lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
-
     #     answer =
     #     print(f'\nAnswer 2 : {answer}\n')
-
     #     # expected =
     #     # self.assertEqual(expected, answer)
 
