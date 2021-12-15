8dfe68fd1ac173e598acc29499b58d6dec53e2b1
Adrian Wilkins <adrian.wilkins@infinityworks.com>
2021-12-15 06:37:56 +0000

    Update code foot

--- templates/python/test_code_foot.py
+++ templates/python/test_code_foot.py
@@ -18,14 +18,14 @@ class TestThing(TestCase):
 
     # def test_answer_1(self):
     #     lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
-    #     answer =
+    #     answer = answer_1(lines)
     #     print(f'\nAnswer 1 : {answer}\n')
     #     # expected =
     #     # self.assertEqual(expected, answer)
 
     # def test_answer_2(self):
     #     lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
-    #     answer =
+    #     answer = answer_2(lines)
     #     print(f'\nAnswer 2 : {answer}\n')
     #     # expected =
     #     # self.assertEqual(expected, answer)
