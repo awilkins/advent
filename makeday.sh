dba014038106adba3e282f419a4fac9639c9dcec
Adrian Wilkins <adrian.wilkins@madetech.com>
2023-11-16 10:35:13 +0000

    Tidies and stuff

--- makeday.sh
+++ makeday.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-DAY_NUM="$1"
 DAYPAD="0$1"
 
 YEAR="$(git name-rev --name-only HEAD | cut -c 1-4)"
@@ -8,8 +7,8 @@ DAY="${DAYPAD: -2}"
 
 NAME=$2
 
-mkdir -p {resources,python/{advent,tests}}/day_${DAY}
-touch python/{advent,tests}/day_${DAY}/__init__.py
+mkdir -p {resources,python/{advent,tests}}"/day_${DAY}"
+touch "python/{advent,tests}/day_${DAY}/__init__.py"
 
 cp templates/python/test_code_head.py "python/tests/day_$DAY/test_$2.py"
 
@@ -23,6 +22,8 @@ cat templates/python/test_code_foot.py >> "python/tests/day_$DAY/test_$2.py"
 
 cp templates/python/code.py "python/advent/day_$DAY/$2.py"
 
-curl "https://adventofcode.com/${YEAR}/day/${1}/input" --cookie "session=$(cat .session_cookie)" > "resources/day_$DAY/input.txt"
+# Get input
+curl "https://adventofcode.com/${YEAR}/day/${1}/input" --cookie "session=${AOC_TOKEN}" > "resources/day_$DAY/input.txt"
 
+# Open VSCode windows on the code we created
 code "python/advent/day_$DAY/$2.py" "python/tests/day_$DAY/test_$NAME.py"
