57030991f9ecf4166e21155ac2c55098903db293
Adrian Wilkins <adrian.wilkins@madetech.com>
2023-11-30 15:37:11 +0000

    Make folder layout simpler

--- makeday.sh
+++ makeday.sh
@@ -19,23 +19,23 @@ then
   exit 1
 fi
 
-mkdir -p {resources,python/{advent,tests}}"/day_${DAY}"
-touch python/{advent,tests}/"day_${DAY}/__init__.py"
+mkdir -p {resources,advent}"/day_${DAY}"
+touch "advent/day_${DAY}/__init__.py"
 
-cp templates/python/test_code_head.py "python/tests/day_$DAY/test_$2.py"
+cp templates/python/test_code_head.py "advent/day_$DAY/test_$2.py"
 
 {
   echo "DAY=\"$DAY\""
   printf "\nfrom advent.day_%s.%s import *\n" "$DAY" "$NAME"
-} >> "python/tests/day_$DAY/test_$2.py"
+} >> "advent/day_$DAY/test_$2.py"
 
-cat templates/python/test_code_foot.py >> "python/tests/day_$DAY/test_$2.py"
+cat templates/python/test_code_foot.py >> "advent/day_$DAY/test_$2.py"
 
 
-cp templates/python/code.py "python/advent/day_$DAY/$2.py"
+cp templates/python/code.py "advent/day_$DAY/$2.py"
 
 # Get input
 curl "https://adventofcode.com/${YEAR}/day/${1}/input" --cookie "session=${AOC_TOKEN}" > "resources/day_$DAY/input.txt"
 
 # Open VSCode windows on the code we created
-code "python/advent/day_$DAY/$2.py" "python/tests/day_$DAY/test_$NAME.py"
+code "advent/day_$DAY/$2.py" "advent/day_$DAY/test_$NAME.py"
