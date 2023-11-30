3c943d0a40f90c7eea5849cc69557eeaf22c2f89
Adrian Wilkins <adrian.wilkins@madetech.com>
2023-11-30 16:10:37 +0000

    Simplified layout and some more comments

--- templates/python/test_code_foot.py
+++ templates/python/test_code_foot.py
@@ -17,14 +17,14 @@ class TestThing(TestCase):
     #     self.assertEqual(expected, actual)
 
     # def test_answer_1(self):
-    #     lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
+    #     lines = get_resource_lines(DAY)
     #     answer = answer_1(lines)
     #     print(f'\nAnswer 1 : {answer}\n')
     #     # expected =
     #     # self.assertEqual(expected, answer)
 
     # def test_answer_2(self):
-    #     lines = get_resource(f'day_{DAY}/input.txt').read_text().splitlines()
+    #     lines = get_resource_lines(DAY)
     #     answer = answer_2(lines)
     #     print(f'\nAnswer 2 : {answer}\n')
     #     # expected =
