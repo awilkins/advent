4178995aa8170041da9c82efd2e64c5cec1447da
Adrian Wilkins <adrian.wilkins@madetech.com>
2023-12-03 23:28:15 +0000

    Updated templates

--- makeday.sh
+++ makeday.sh
@@ -25,8 +25,9 @@ touch "advent/day_${DAY}/__init__.py"
 cp templates/python/test_code_head.py "advent/day_$DAY/test_$2.py"
 
 {
-  echo "DAY=\"$DAY\""
   printf "\nfrom advent.day_%s.%s import *\n" "$DAY" "$NAME"
+  echo
+  echo "DAY=\"$DAY\""
 } >> "advent/day_$DAY/test_$2.py"
 
 cat templates/python/test_code_foot.py >> "advent/day_$DAY/test_$2.py"
