d709d0c65d53ded0e33d0c74e9c1d7be7c23c2ce
Adrian Wilkins <adrian.wilkins@madetech.com>
2023-12-03 16:17:56 +0000

    Nasty object wrappers for day 3

--- advent/day_03/test_gondola.py
+++ advent/day_03/test_gondola.py
@@ -77,32 +77,50 @@ class TestPartOne:
         [
             (EXAMPLE_ONE[0], [(467, 0), (114, 5)]),
             (EXAMPLE_ONE[1], []),
-            (".....123", [ (123, 5) ]),
+            (".....123", [(123, 5)]),
         ],
     )
     def test_find_number_groups(self, input, expected):
         assert find_number_groups(input) == expected
 
-    ALL_NUMBERS =  get_all_numbers(EXAMPLE_ONE)
+    ALL_NUMBERS = get_all_numbers(EXAMPLE_ONE)
 
-    NUMBERS_WITH_SYMBOLS = [number for number in ALL_NUMBERS if number[0] not in [114, 58]]
+    NUMBERS_WITH_SYMBOLS = [
+        number for number in ALL_NUMBERS if number[0] not in [114, 58]
+    ]
+
+    # @pytest.mark.skip
+    @pytest.mark.parametrize("input", NUMBERS_WITH_SYMBOLS)
+    def test_component_has_adjacent_symbol(self, input):
+        c = Component()
+        c.number, c.x, c.y = input[0], input[1][0], input[1][1]
+        assert c.has_adjacent_symbol(EXAMPLE_ONE)
 
     # @pytest.mark.skip
     @pytest.mark.parametrize("input", NUMBERS_WITH_SYMBOLS)
     def test_has_adjacent_symbol(self, input):
         assert has_adjacent_symbol(EXAMPLE_ONE, input)
 
-    @pytest.mark.parametrize("input", [
-        (114, (5, 0)),
-        (58, (7, 5))
-    ])
+    @pytest.mark.parametrize("input", [(114, (5, 0)), (58, (7, 5))])
     def test_has_no_adjacent_symbol(self, input):
         assert not has_adjacent_symbol(EXAMPLE_ONE, input)
+
+    @pytest.mark.parametrize("input", [(114, (5, 0)), (58, (7, 5))])
+    def test_component_has_no_adjacent_symbol(self, input):
+        c = Component()
+        c.number, c.x, c.y = input[0], input[1][0], input[1][1]
+        assert not c.has_adjacent_symbol(EXAMPLE_ONE)
 
     def test_example_1(self):
         lines = EXAMPLE_ONE
         expected = 4361
         actual = answer_1(lines)
+        assert expected == actual
+
+    def test_example_1_c(self):
+        lines = EXAMPLE_ONE
+        expected = 4361
+        actual = answer_1_c(lines)
         assert expected == actual
 
     @pytest.mark.parametrize(
@@ -135,9 +153,8 @@ class TestPartOne:
         lines = get_resource_lines(DAY)
         answer = answer_1(lines)
         print(f"\nAnswer 1 : {answer}\n")
-        # expected =
-        # assert expected == answer
-        assert answer != 541476
+        expected = 544664
+        assert expected == answer
 
 
 EXAMPLE_TWO = EXAMPLE_ONE
@@ -146,15 +163,15 @@ EXAMPLE_TWO = EXAMPLE_ONE
 class TestPartTwo:
     pass
 
-    # def test_example_2(self):
-    #     lines = EXAMPLE_TWO.splitlines()
-    #     expected =
-    #     actual = answer_2(lines)
-    #     assert expected == actual
+    def test_example_2(self):
+        lines = EXAMPLE_TWO
+        expected = 467835
+        actual = answer_2(lines)
+        assert expected == actual
 
-    # def test_answer_2(self):
-    #     lines = get_resource_lines(DAY)
-    #     answer = answer_2(lines)
-    #     print(f'\nAnswer 2 : {answer}\n')
-    #     # expected =
-    #     # assert expected == answer
+    def test_answer_2(self):
+        lines = get_resource_lines(DAY)
+        answer = answer_2(lines)
+        print(f'\nAnswer 2 : {answer}\n')
+        # expected =
+        # assert expected == answer
