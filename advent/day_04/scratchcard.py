8b0450994147142758dee2dc00fab845d6bbf171
Adrian Wilkins <adrian.wilkins@madetech.com>
2023-12-04 08:29:49 +0000

    Day 4

---
+++ advent/day_04/scratchcard.py
@@ -0,0 +1,57 @@
+from collections import deque
+
+from typing import Sequence, List
+
+class Card:
+
+    number: int
+    winners: List[int] = []
+    scorers: List[int] = []
+
+    def __init__(self, line: str):
+        parts = line.split('|')
+        number, winners = parts[0].split(':')
+        self.number = int(number.split()[1])
+        self.winners = list(int(w) for w in winners.split())
+        self.scorers = list(int(s) for s in parts[1].split())
+
+    def score(self):
+        score = 0
+        for number in self.scorers:
+            if number in self.winners:
+                score = max(1, score * 2)
+        return score
+
+    def matching(self):
+        score = 0
+        for number in self.scorers:
+            if number in self.winners:
+                score += 1
+        return score
+
+
+
+def answer_1(lines: Sequence[str]):
+    cards = [Card(line) for line in lines]
+    return sum(card.score() for card in cards)
+
+
+def answer_2(lines: Sequence[str]):
+    cards = list(Card(line) for line in lines)
+
+    card_pile = []
+    card_queue = deque(cards)
+
+    while len(card_queue):
+        current_card = card_queue.popleft()
+        matches = current_card.matching()
+
+        index = current_card.number # this works because Card 1 is at index 0
+        end = index + matches
+        # copy original cards
+        card_queue.extend(cards[index:end])
+
+        # put current card on pile
+        card_pile.append(current_card)
+
+    return len(card_pile)
---
+++ advent/day_04/test_scratchcard.py
@@ -0,0 +1,59 @@
+import pytest
+
+from ..util import get_resource_lines
+DAY="04"
+
+from advent.day_04.scratchcard import *
+
+EXAMPLE_ONE = """\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+""".splitlines()
+
+class TestPartOne:
+    pass
+
+    @pytest.mark.parametrize('line,score', [
+        (EXAMPLE_ONE[0], 8),
+        (EXAMPLE_ONE[1], 2),
+        (EXAMPLE_ONE[2], 2),
+        (EXAMPLE_ONE[3], 1),
+        (EXAMPLE_ONE[4], 0),
+        (EXAMPLE_ONE[5], 0),
+    ])
+    def test_example_1(self, line, score):
+        card = Card(line)
+        assert card.score() == score
+        assert card.number > 0
+
+
+    def test_answer_1(self):
+        lines = get_resource_lines(DAY)
+        answer = answer_1(lines)
+        print(f'\nAnswer 1 : {answer}\n')
+        # expected =
+        # assert expected == answer
+
+
+EXAMPLE_TWO = EXAMPLE_ONE
+
+class TestPartTwo:
+    pass
+
+    def test_example_2(self):
+        lines = EXAMPLE_TWO
+        expected = 30
+        actual = answer_2(lines)
+        assert expected == actual
+
+    def test_answer_2(self):
+        lines = get_resource_lines(DAY)
+        answer = answer_2(lines)
+        print(f'\nAnswer 2 : {answer}\n')
+        # expected =
+        # assert expected == answer
+
