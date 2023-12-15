236e881114a7329c143b82b1b042d5008259138f
Adrian Wilkins <adrian.wilkins@madetech.com>
2023-12-15 07:57:45 +0000

    Day 15

---
+++ advent/day_15/lens_library.py
@@ -0,0 +1,87 @@
+from __future__ import annotations
+
+from collections import deque
+
+from typing import Sequence, List, NamedTuple
+
+
+class Lens(NamedTuple):
+    label: str
+    focal: int
+
+
+class HashMap:
+
+    boxes: list[deque[Lens]]
+
+    def __init__(self):
+        self.boxes = []
+        for ii in range(256):
+            self.boxes.append(deque())
+
+    def __str__(self):
+        lines = []
+        for index, box in enumerate(self.boxes):
+            if box:
+                lines.append(f"Box {index}: [{'] ['.join([f'{l.label} {l.focal}' for l in box])}]")
+        return "\n".join(lines)
+
+    def step(self, line:str):
+        if line[-1].isdigit():
+            focal = int(line[-1])
+            label = line[:-2]
+            lens = Lens(label, focal)
+            self.add(lens)
+        else:
+            label = line[:-1]
+            self.remove(label)
+
+    def remove(self, label: str):
+        box = self.boxes[hash(label)]
+        for index, value in enumerate(box):
+            if value.label == label:
+                del box[index]
+                return
+
+    def add(self, lens: Lens):
+        box = self.boxes[hash(lens.label)]
+        for index, value in enumerate(box):
+            if value.label == lens.label:
+                box[index] = lens
+                return
+        box.append(lens)
+
+    def focal_power(self):
+
+        def lens_power(lens: Lens, box_index: int, slot_index: int):
+            return (1 + box_index) * (slot_index + 1) * lens.focal
+
+        total_power = 0
+        for box_index, box in enumerate(self.boxes):
+            for slot_index, slot in enumerate(box):
+                total_power += lens_power(slot, box_index, slot_index)
+
+        return total_power
+
+
+def hash(line: str) -> int:
+    current = 0
+    for c in line:
+        code = ord(c)
+        current += code
+        current *= 17
+        current %= 256
+    return current
+
+
+def answer_1(lines: Sequence[str]):
+    return sum(hash(step) for step in lines[0].split(","))
+
+
+def answer_2(lines: Sequence[str]):
+    hashmap = HashMap()
+    for step in lines[0].split(','):
+        hashmap.step(step)
+
+    return hashmap.focal_power()
+
---
+++ advent/day_15/test_lens_library.py
@@ -0,0 +1,52 @@
+import pytest
+
+from ..util import get_resource_lines
+
+
+from advent.day_15.lens_library import *
+
+DAY = "15"
+
+EXAMPLE_ONE = """\
+rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7
+""".splitlines()
+
+
+class TestPartOne:
+    pass
+
+    def test_hash(self):
+        assert hash("HASH") == 52
+
+    # def test_example_1(self):
+    #     lines = EXAMPLE_ONE
+    #     expected =
+    #     actual = answer_1(lines)
+    #     assert expected == actual
+
+    def test_answer_1(self):
+        lines = get_resource_lines(DAY)
+        answer = answer_1(lines)
+        print(f"\nAnswer 1 : {answer}\n")
+        # expected =
+        # assert expected == answer
+
+
+EXAMPLE_TWO = EXAMPLE_ONE
+
+
+class TestPartTwo:
+    pass
+
+    def test_example_2(self):
+        lines = EXAMPLE_TWO
+        expected = 145
+        actual = answer_2(lines)
+        assert expected == actual
+
+    def test_answer_2(self):
+        lines = get_resource_lines(DAY)
+        answer = answer_2(lines)
+        print(f'\nAnswer 2 : {answer}\n')
+        # expected =
+        # assert expected == answer
